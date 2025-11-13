6591277b15f1686c3575ca3adc2799d027634941
Jordan Gonzalez <gonzalezjordan61@gmail.com>
2025-11-13 01:45:21 -0600

    Cambio error cors

--- app.js
+++ app.js
@@ -42,7 +42,6 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // Middleware para CORS
-const cors = require("cors");
 
 app.use(
   cors({
