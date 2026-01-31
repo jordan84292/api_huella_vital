f3abf67a5b5ad90c2da3db96839a7761807bb139
Jordan Gonzalez <gonzalezjordan61@gmail.com>
2026-01-30 19:30:52 -0600

    se cambia el cors a vercel

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://huella-vital-veterinaria.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
